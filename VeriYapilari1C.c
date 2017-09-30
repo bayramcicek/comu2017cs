492f5aa720154fd4460ce827d721a858b6bf8096
Bayram ÇİÇEK <bayramcicek17@gmail.com>
2017-09-30 03:13:50 +0300

    language : C

---
+++ VeriYapilari1C.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+void dizi_yaz(int *dizi, int size){
+    int i;
+    for(i=0; i<size; i++)
+        printf("%4d", *(dizi+i));
+    printf("\n\n");
+  //  for(i=0; i<size; i++)
+    //    printf("%6d", *(dizi+i));
+   // printf("\n");
+}
+
+void dizi_yaz1(int (*dizi)[2],int size){
+    int i,j;
+    for(i=0; i<size; i++){
+        for(j=0; j<2; j++)
+           printf("%4d",*(*(dizi+i)+j));
+          //  printf("%4d",dizi[i][j]);    
+        printf("\n");
+    }
+    
+}
+
+void dizi_yaz2(int (*dizi)[3][5], int size){
+    int i,j,k;
+    
+    for(i=0; i<size; i++){
+        for(j=0; j<3; j++){
+            for(k=0; k<5; k++)
+                printf("%4d",dizi[i][j][k]);
+            printf("\n");    
+        }
+        printf("\n");
+    }               
+}                
+                
+ 
+void matris_carpimi(int A[][3], int A_satir, int A_sutun,
+                    int B[][2], int B_satir,int B_sutun,
+                    int C[][2],int C_satir, int C_sutun){
+    int i,j,k;
+    if(A_sutun!=B_satir){
+        printf("Carpim matrislerinin boyutlari uyusmuyor!!!");
+        return;
+    }
+    else if(A_satir!=C_satir || B_sutun!=C_sutun){
+        printf("Sonuc matrisinin boyutlari carpan matrislere uymuyor!!!\n");
+        return;
+    }
+    else{
+        for(i=0; i<A_satir; i++)
+            for(j=0; j<B_sutun; j++){
+                C[i][j]=0;
+                for(k=0; k<A_sutun; k++)
+                    C[i][j]=C[i][j]+A[i][k]*B[k][j];
+            }   
+    }
+}
+
+
+                
+int main(int argc, char** argv) {
+    int A[]={0,1,12,33,4,5,6,7,8,9};
+    int B[][5]={{0,1,2,3,4},
+                 {10,877,12,13,14},
+                 {20,21,22,23,24},
+                 {30,31,32,33,34}};
+    
+    int C[2][3][5]= {{{0,11,2,3,4},
+                 {10,877,12,13,14},
+                 {20,21,22,23,24}},
+                 {{0,1,2,3,4},
+                 {10,877,152,13,14},
+                 {20,21,22,23,274}}};
+                 
+    int AA[][3]={{1,8,1},{2,1,4},{5,1,0},{1,2,3}};
+    int BB[][2]={{0,1},{2,4},{3,9}};
+    int CC[4][2];
+    
+    //dizi_yaz(&C[1][1][1], 7);
+   // dizi_yaz2(C+1, 1);
+    matris_carpimi(AA,4,3,BB,3,2,CC,4,2);
+    dizi_yaz1(CC,4);
+    //dizi_yaz1(CC,4);
+    
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ VeriYapilari2C.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+
+void tek_boyutlu_dizi_olustur(int **dizi, int eleman_sayisi){
+    int i;
+    *dizi=(int*)malloc(sizeof(int)*eleman_sayisi);
+
+    for(i=0; i<eleman_sayisi; i++){
+        *(*dizi+i)= rand()%10;
+    }
+}
+
+void tek_boyutlu_dizi_yaz(int *dizi, int eleman_sayisi){
+    int i;
+
+    for(i=0; i<eleman_sayisi; i++){
+         printf("0x%p ", (dizi+i));
+    }
+    printf("\n");
+    for(i=0; i<eleman_sayisi; i++){
+         printf("%10d ", *(dizi+i));
+    }
+    printf("\n");
+}
+
+void dinamik_matris_olustur(int ***dizi,int satir,int sutun){
+    int i,j;
+    *dizi=(int**)malloc(satir*sizeof(int*));
+    for(i=0; i<satir; i++){
+        *(*dizi+i)= (int*)malloc(sutun*sizeof(int));
+        for(j=0; j<sutun; j++)
+            *(*(*dizi+i)+j)= rand()%3;
+    }
+}
+
+void matris_degistir(int **dizi, int satir, int sutun){
+    int i,j;
+    for(i=0; i<satir; i++)
+        for(j=0; j<sutun; j++)
+            *(*(dizi+i)+j)=rand()%2;
+}
+
+
+void dinamik_matris_yaz(int **dizi,int satir,int sutun){
+    int i,j;
+    for(i=0; i<satir; i++){
+        for(j=0; j<sutun; j++) printf("%4d ",*(*(dizi+i)+j));
+        printf("\n");
+    }
+}
+
+void dinamik_matris_carpimi(int **A,int A_satir, int A_sutun,
+                           int **B, int B_satir, int B_sutun,
+                           int **C, int C_satir, int C_sutun){
+
+
+    int i,j,k;
+    if(A_sutun!=B_satir){
+        printf("Carpim matrislerinin boyutlari uyusmuyor!!!");
+        return;
+    }
+    else if(A_satir!=C_satir || B_sutun!=C_sutun){
+        printf("Sonuc matrisinin boyutlari carpan matrislere uymuyor!!!\n");
+        return;
+    }
+    else{
+        for(i=0; i<A_satir; i++)
+            for(j=0; j<B_sutun; j++){
+                C[i][j]=0;
+                for(k=0; k<A_sutun; k++)
+                    C[i][j]=C[i][j]+A[i][k]*B[k][j];
+
+            }
+    }
+}
+
+int main(int argc, char** argv) {
+    int **A,**B,**C;
+    int *dizi;
+    int eleman_sayisi;
+    srand(time(NULL)); // rastgele (random) sayilar olusturmak icin
+  /*  eleman_sayisi=10;
+    tek_boyutlu_dizi_olustur(&dizi, eleman_sayisi);
+    tek_boyutlu_dizi_yaz(dizi, eleman_sayisi); */
+    dinamik_matris_olustur(&A,4,3);
+    printf("--- A matrisi ----\n");
+    dinamik_matris_yaz(A,4,3);
+
+    A[0][1]=100;
+    printf("--- A matrisi ----\n");
+    dinamik_matris_yaz(A,4,3);
+
+    dinamik_matris_olustur(&B,3,5);
+    printf("--- B matrisi ----\n");
+    dinamik_matris_yaz(B,3,5);
+
+    dinamik_matris_olustur(&C,4,5);
+   // printf("--- C matrisi ----\n");
+  //  dinamik_matris_yaz(C,5,7);
+
+    dinamik_matris_carpimi(A,4,3,B,3,5,C,4,5);
+    printf("---- C=A*B matrisi ----\n");
+    dinamik_matris_yaz(C,4,5);
+    printf("-----------------------\n");
+
+
+    return (EXIT_SUCCESS);
+}
